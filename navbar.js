f798e6324f6edbcb17203ba459fb885d62345da4
haryp2309 <hary2309@gmail.com>
2018-09-25 12:33:14 +0200

    new

---
+++ navbar.js
@@ -0,0 +1,21 @@
+document.write('\
+\
+	<div id="navbar">
+		<div id="navbar-logo">
+		<img src="images/Logo.jpg" width="100px">
+		</div>
+		
+		<div id="navbar-textlogo">
+		<p>#haryp</p>
+		</div>
+		
+		<div id="navbar-title">
+		<h1>
+		Welcome!
+		</h1>
+		<h2 class="undertitle">
+			to #haryp's official website 
+		</h2>
+		</div>
+	</div>\
+	');
