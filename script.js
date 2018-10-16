81e4a7baa324fb557186c2002cc0a8af2e5583dc
haryp2309 <hary2309@gmail.com>
2018-10-16 10:49:37 +0200

    JavaScript Link

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.body.className += 'fade-out';
 
- function fadeOut() {
+function fadeOut() {
 	var bodyEl = document.getElementsByTagName("body")[0];
 		bodyEl.classList.remove("fade-out");
 }
 
 window.onload=fadeOut;
+
+function openContactMe() {
+	window.open("contact-info.html", "_self")
+}
+
+function openVideoGallery() {
+	window.open("video-gallery.html", "_self")
+}
+
+function fadeOutAnimation() {
+	var bodyEl = document.getElementsByTagName("body")[0];
+		bodyEl.classList.add("fade-out-animation");
+}
