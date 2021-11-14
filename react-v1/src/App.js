e6ed66e54ac69a5ea26a8c07ce908dea3ee92987
haryp2309 <hary2309@gmail.com>
2021-11-14 01:38:31 +0100

    Updated old react app to redirect to new app.

--- react-v1/src/App.js
+++ react-v1/src/App.js
@@ -7,16 +7,30 @@ import { darkTheme } from "./components/colorPalette";
 import { Container, CssBaseline } from "@material-ui/core";
 import RepoCardContainer from "./components/repoCardContainer";
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Navbar />
-      <Container maxWidth="lg">
-        <RepoCardContainer />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/old">
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Navbar />
+            <Container maxWidth="lg">
+              <RepoCardContainer />
+            </Container>
+          </ThemeProvider>
+        </Route>
+        <Route
+          path="/"
+          component={() => {
+            window.location.href = "https://haryp2309.vercel.app/";
+            return null;
+          }}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
