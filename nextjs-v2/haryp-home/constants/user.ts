d5312a130ab05265832c240a23c1e9f0a8be657f
haryp2309 <hary2309@gmail.com>
2022-10-16 15:27:49 -0700

    Update user.ts

--- nextjs-v2/haryp-home/constants/user.ts
+++ nextjs-v2/haryp-home/constants/user.ts
@@ -5,6 +5,6 @@ export const USER_LINK = `https://github.com/${USER_ID}`;
 export const getApiBaseURL = (id: string) =>
   `https://api.github.com/users/${id}`;
 
-export const ORGANISATIONS = ["Garasje-folka"];
+export const ORGANISATIONS = ["Garasje-folka", "modern-daw"];
 
 export const LINKED_IN_URL = "https://no.linkedin.com/in/haryp";
